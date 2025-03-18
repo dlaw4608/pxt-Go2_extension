f2faab8520e34f7a13e9083c31318b449934bf2d
Daniel Lawton <dlawton032@gmail.com>
2025-03-18 21:21:37 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Sit")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Sit") {
+        serial.writeString("Sit")
+    } else if (receivedString == "RiseSit") {
+        serial.writeString("RiseSit")
+        basic.showString("RiseSit")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("RiseSit")
+})
+radio.setGroup(1)
