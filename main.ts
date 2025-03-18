f3338aebb5c6bc1d8aed761f34e1bb80adb767d4
Daniel Lawton <dlawton032@gmail.com>
2025-03-18 21:37:24 +0000

    testing dog sit

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* 
 Go2 Test
 
-//& blockId = Go2_Sit
-//& block = "Tell Dog to Sit"
+//% blockId = Go2_Sit
+//% block = "Tell Dog to Sit"
 */
 function sit_command() {
     radio.sendString("Sit")
